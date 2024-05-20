babef2e18f50177b0a0ff8138a0d3ee128ffdb4e
abdelrahman <abdelrahmanabdellatif0@gmail.com>
2024-05-20 03:31:46 +0300

    Write reset() + add ability to change canvas size

--- code.js
+++ code.js
@@ -1,16 +1,45 @@
-const canvas = document.querySelector("#canvas")
-const input = document.querySelector("input")
-const btn = document.querySelector("button")
-let size = 16
+const canvas = document.querySelector("#canvas");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const canvasStyle = getComputedStyle(canvas);
+let canvasSize = canvasStyle.getPropertyValue("width");
+canvasSize = +(canvasSize.split("p").shift())
 
+console.log(btn.textContent)
+btn.addEventListener('click', reset);
+let PixelCount = 16;
 
-
-function makeGrid(size) {
-    for (let i = 0; i < (size*size); i++) {
-        pixel = document.createElement("div")
-        pixel.classList.add("grid")
-        canvas.appendChild(pixel)
+function makeGrid(pixelCount) {
+    for (let i = 0; i < (PixelCount*PixelCount); i++) {
+        pixelSize = canvasSize / PixelCount;
+        const pixel = document.createElement("div");
+        pixel.classList.add("grid");
+        pixel.setAttribute("style", `width:${pixelSize}px; height:${pixelSize}px;`)
+        canvas.appendChild(pixel);
     }
 }
 
-makeGrid(size)
+
+function reset() {
+    console.log("click")
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild)
+    }
+    if (input.value === '') {
+        makeGrid(PixelCount);
+    }
+    else {
+        if (input.value < 16 || input.value > 100) {
+            alert("Please Enter a number ranging from 16 to 100")
+            makeGrid();
+        }
+        else {
+            PixelCount = input.value;
+            makeGrid(PixelCount);
+        }
+    }
+    input.value = '';
+}
+
+
+makeGrid(PixelCount);
